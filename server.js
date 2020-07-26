065b4918408fa8f2a97c1a47d6d1677ff5d01c48
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-07-26 23:18:50 +0900

    Add node.js express

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/api/customers', (req,res) => {
+  res.send([{
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '990102',
+    'gender': '여자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '김현우',
+    'birthday': '150415',
+    'gender': '남자',
+    'job': '유치원생'
+  }]);
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
